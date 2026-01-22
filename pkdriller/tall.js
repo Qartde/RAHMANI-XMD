05134f9dc7ae231da4a294b26cc71e0989ac42fa
Qartde <bonifacemd2525@gmail.com>
2026-01-22 21:17:06 +0300

    Update tall.js

--- pkdriller/tall.js
+++ pkdriller/tall.js
@@ -1,10 +1,68 @@
-const axios = require('axios');
-const hans = "https://sever-ten.vercel.app/api/🤠";
-axios.get(hans)
-    .then(response => {
-        console.log("\x1b[32m✅ Successfully loaded script From HansTz Sever.\x1b[0m");
-        eval(response.data);
-    })
-    .catch(err => {
-        console.error("\x1b[31m❌ Failed to load script from HansTz Sever API. Error:", err.message, "\x1b[0m");
-    });
+const { zokou } = require('../framework/zokou');
+const { default: axios } = require('axios');
+
+if (!global.userChats) global.userChats = {};
+
+zokou({ nomCom: "gpt", reaction: "🤖", categorie: "ai" }, async (dest, zk, commandeOptions) => {
+    const { arg, ms } = commandeOptions;
+    const sender = ms.sender;
+    const from = dest;
+
+    try {
+        if (!arg || arg.length === 0) {
+            return await zk.sendMessage(from, { text: "🚫 Please provide a question or message.🚫" }, { quoted: ms });
+        }
+
+        const text = arg.join(" ");
+
+        // Initialize user chat history
+        if (!global.userChats[sender]) global.userChats[sender] = [];
+        global.userChats[sender].push(`User: ${text}`);
+
+      
+        if (global.userChats[sender].length > 15) global.userChats[sender].shift();
+
+        const history = global.userChats[sender].join("\n");
+
+        const prompt = `
+You are Rahmany Ai,your creator, a friendly and intelligent WhatsApp bot. Chat naturally without asking repetitive questions.
+
+### Chat History:
+${history}
+`;
+
+       
+        const { data } = await axios.get("https://omegatech-api.dixonomega.tech/api/ai/Claude", {
+            params: { text: text }
+        });
+
+      
+        const botResponse = data?.result?.text || "⚠️ Sorry, I couldn't understand your question.";
+
+        // Save bot reply in history
+        global.userChats[sender].push(`Bot: ${botResponse}`);
+
+        // Send plain reply
+        await zk.sendMessage(from, { text: botResponse }, { quoted: ms });
+
+    } catch (err) {
+        console.error("❌ GPT Error:", err);
+        await zk.sendMessage(from, { text: "❌ An error occurred: " + err.message }, { quoted: ms });
+    }
+});
+image: { url: image },
+//     gifPlayback: true,
+     caption: teks,
+     buttons: buttons,
+     contextInfo: {
+       forwardingScore: 999,
+         isForwarded: true,
+         forwardedNewsletterMessageInfo: {
+           newsletterJid: "120363353854480831@newsletter",
+             newsletterName: "ʀᴀʜᴍᴀɴɪ xᴍᴅ"
+            }
+        },
+        footer: "ʀᴀʜᴍᴀɴɪ",
+        viewOnce: true,
+        headerType: 6
+   }
