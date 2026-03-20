c6a469bd54ba68fe73e70623d4689743e348a036
Qartde <bonifacemd2525@gmail.com>
2026-03-20 06:06:04 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -1,9 +1,5 @@
 var tabCmds = [];
 let cm = [];
-
-// Store event handlers
-let eventHandlers = [];
-
 function zokou(obj, fonctions) {
     let infoComs = obj;
     if (!obj.categorie) {
@@ -17,32 +13,4 @@ function zokou(obj, fonctions) {
     // console.log('chargement...')
     return infoComs;
 }
-
-// Function to register event handlers (always running)
-function onEvent(handler) {
-    eventHandlers.push(handler);
-}
-
-// Function to handle incoming messages (called from index.js)
-async function handleMessage(zk, message) {
-    try {
-        // Execute all registered event handlers
-        for (const handler of eventHandlers) {
-            try {
-                await handler(zk, message);
-            } catch (e) {
-                console.error("Event handler error:", e);
-            }
-        }
-    } catch (e) {
-        console.error("Handle message error:", e);
-    }
-}
-
-module.exports = { 
-    zokou, 
-    Module: zokou, 
-    cm,
-    onEvent,
-    handleMessage
-};
+module.exports = { zokou, Module: zokou, cm };
